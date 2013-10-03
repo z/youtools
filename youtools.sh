d36c3456b457c1e07099fedd17a05d8d7e0a507d
Tyler '-z-' Mulligan <z@interwebninja.com>
2013-10-03 19:06:18 -0400

    Updates to script and case switch

--- youtools.sh
+++ youtools.sh
@@ -51,8 +51,20 @@ youtube_to_mp3() {
 	mplayer -dumpaudio $1 -dumpfile ${1%.*}.mp3
 }
 youtube_to_mp3_batch() {
+    #mediainfo --Inform="Audio;%BitRate/String%"
+    #for f in *.{mp4,flv}; do echo -e $f; mediainfo --Inform="Audio;%BitRate/String%" $f; done
+    #youtube-dl -t -f 35 --extract-audio --audio-format=mp3 http://www.youtube.com/watch?v=6g6g2mvItp4
+	#youtube-dl -t -f 35 -a videos.txt
 	youtube-dl -t -a videos.txt
-	for f in *.flv,*.mp4; do mplayer -dumpaudio $f -dumpfile ${f/%${f##*.}}; done
+	#for f in *.mp4; do mplayer -dumpaudio $f -dumpfile ${f%.*}.mp3; done
+	for f in *.{flv,mp4}; do
+        artist=$(echo $f | awk -F "-" '{ print $1 }' | sed 's/_/ /g;s/^[ \t]*//;s/[ \t]*$//')
+        title=$(echo $f | awk -F "-" '{ print $2 }' | sed 's/_/ /g;s/^[ \t]*//;s/[ \t]*$//')
+        #echo ffmpeg -i $f -f mp3 -ab 192000 -metadata title="$title" -metadata artist="$artist" ${f%.*}.mp3
+        echo ffmpeg -i $f -f mp3 -ab 192000 -metadata title="$title" -metadata artist="$artist" ${f%.*}.mp3
+        #ffmpeg -i $f -f mp3 -ab 192000 -metadata title="$title" -metadata artist="$artist" ${f%.*}.mp3
+        ffmpeg -i $f -f mp3 -ab 192000 -metadata title="$title" -metadata artist="$artist" ${f/%${f##*.}}mp3
+    done
 }
 
 video_to_mp3() {
@@ -69,8 +81,9 @@ shift
 case $select in
   --youtube_to_gif|-y) youtube_to_gif $@;;
   --video_to_gif|-v) video_to_gif $@;;
-  --youtube_to_mp3|-t) video_to_gif $@;;
-  --video_to_mp3|-m) video_to_gif $@;;
+  --youtube_to_mp3_batch|-b) youtube_to_mp3_batch;;
+  --youtube_to_mp3|-t) youtube_to_mp3 $@;;
+  --video_to_mp3|-m) video_to_mp3 $@;;
   --help|-h|*) h;;
 esac
 
